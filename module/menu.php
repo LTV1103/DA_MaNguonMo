af95ac48fe6d3986cf2fb1ce926fd73c1a77eb1b
Lương Triều Vỹ <146475846+LTV1103@users.noreply.github.com>
2024-12-05 01:14:42 +0700

    thiet ke giao dien nguoi dung

---
+++ module/menu.php
@@ -0,0 +1,9 @@
+<div class="menu">
+            <ul class="list_menu">
+                <li><a href="index.php">Trang Chủ</a></li>
+                <li><a href="index.php?quanly=danhmucsanpham">Sản Phẩm</a></li>
+                <li><a href="index.php?quanly=giohang">Giỏ Hàng</a></li>
+                <li><a href="index.php?quanly=lienhe">Liên Hệ</a></li>
+                <li><a href="admin/">Admin</a></li>
+            </ul>
+        </div>
